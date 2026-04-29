feb3006463bd2806628abc637da5bcb8df3f8484
EaseDo <111581553+DanielSun024@users.noreply.github.com>
2026-04-29 16:26:47 +0800

    Update +Page.vue

--- pages/index/+Page.vue
+++ pages/index/+Page.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen relative pb-20">
     <div class="fixed inset-0 z-0">
-      <img src="https://images.unsplash.com/photo-1614850523296-d8c1af93d400?q=80&w=2070" class="w-full h-full object-cover" />
+      <img src="https://api.yppp.net/api.php" class="w-full h-full object-cover" />
       <div class="absolute inset-0 bg-black/5 backdrop-brightness-90"></div> 
     </div>
 
@@ -15,7 +15,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="size-6 text-yellow-400 animate-pulse" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
             </svg>
-            <span class="text-xl uppercase">公告 / Notice</span>
+            <span class="text-xl uppercase">公告</span>
           </div>
 
           <div class="border-t border-dashed border-white/20 mb-8 w-full"></div>
